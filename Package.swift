b8a930870c0567407693d51aa677d546e11255e2
Nikita Patskov <patskovn@yahoo.com>
2021-01-27 13:01:19 +0300

    Back

--- Package.swift
+++ Package.swift
@@ -6,11 +6,11 @@ let package = Package(
     platforms: [.macOS("10.10")],
     // platforms: [.iOS("9.0"), .macOS("10.10"), tvOS("9.0"), .watchOS("2.0")],
     products: [
-        .library(name: "LottieMac", targets: ["LottieMac"])
+        .library(name: "Lottie", targets: ["Lottie"])
     ],
     targets: [
         .target(
-            name: "LottieMac",
+            name: "Lottie",
             path: "lottie-swift/src",
             exclude: ["Public/iOS"]
         )
